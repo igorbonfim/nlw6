b478765aafd5a0ec070ceeedf5887d193135b429
Igor Bonfim <igorbonfim88@gmail.com>
2021-07-09 22:57:08 -0300

    Aula 3

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,18 +1,29 @@
+import { useParams } from 'react-router-dom';
+
 import logoImg from '../assets/images/logo.svg';
+
 import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+
 import '../styles/room.scss';
 
+type RoomParams = {
+    id: string;
+}
+
 export function Room(){
+    const params = useParams<RoomParams>();
+
     return (
         <div id="page-room">
             <header>
                 <div className="content">
-                    <img src={logoImg} alt="" />
-                    <div>codigo</div>
+                    <img src={logoImg} alt="Letmeask" />
+                    <RoomCode code={params.id} />
                 </div>
             </header>
 
-            <main className="content">
+            <main>
                 <div className="room-title">
                     <h1>Sala React</h1>
                     <span>4 perguntas</span>
