fbb86d7032ff0d24858ff34ce784b893816788a8
Igor Bonfim <igorbonfim88@gmail.com>
2021-08-07 11:56:24 -0300

    Aula 4

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,4 +1,3 @@
-import { FormEvent, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
@@ -6,9 +5,8 @@ import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
-import { useAuth } from '../hooks/useAuth';
+//import { useAuth } from '../hooks/useAuth';
 import { useRoom } from '../hooks/useRoom';
-import { database } from '../services/firebase';
 
 import '../styles/room.scss';
 
@@ -38,38 +36,11 @@ type RoomParams = {
 }
 
 export function AdminRoom(){
-    const { user } = useAuth();
-    const params = useParams<RoomParams>();
-    const [newQuestion, setNewQuestion] = useState('');  
+    //const { user } = useAuth();
+    const params = useParams<RoomParams>();     
     const roomId = params.id;
     
-    const { title, questions } = useRoom(roomId);     
-
-    async function handleSendQuestion(event: FormEvent) {
-        event.preventDefault();
-
-        if (newQuestion.trim() === '') {
-            return;
-        }
-
-        if (!user) {
-           throw new Error('You must me logged in');
-        }
-
-        const question = {
-            content: newQuestion,
-            author: {
-                name: user.name,
-                avatar: user.avatar
-            },            
-            isHighlighted: false,
-            isAnswered: false
-        };
-
-        await database.ref(`rooms/${roomId}/questions`).push(question);
-
-        setNewQuestion('');
-    }   
+    const { title, questions } = useRoom(roomId);  
 
     return (
         <div id="page-room">
@@ -87,27 +58,7 @@ export function AdminRoom(){
                 <div className="room-title">
                     <h1>Sala {title}</h1>
                     { questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
-                </div>
-
-                <form onSubmit={handleSendQuestion}>
-                    <textarea 
-                        placeholder="O que você quer perguntar?"
-                        onChange={event => setNewQuestion(event.target.value)}
-                        value={newQuestion}
-                    />
-
-                    <div className="form-footer">
-                        { user ? (
-                            <div className="user-info">
-                                <img src={user.avatar} alt={user.name} />
-                                <span>{user.name}</span>
-                            </div>
-                        ) : (
-                            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
-                        )  }
-                        <Button type="submit" disabled={!user}>Enviar pergunta</Button>                        
-                    </div>                     
-                </form>
+                </div>                
                 
                 <div className="question-list">
                     {questions.map(question => {
