52878a4e9db09be28d500f358d4dc6ab3d5772c1
Igor Bonfim <igorbonfim88@gmail.com>
2021-08-08 15:11:21 -0300

    Ajustes aula 4

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -40,7 +40,7 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
         })
 
         return () => {
-        unsubscribe();
+            unsubscribe();
         }
     }, [])
 
